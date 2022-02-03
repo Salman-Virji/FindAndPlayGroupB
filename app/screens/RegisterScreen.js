31d7f3e357c7a80c57cc8708b30c39b42b4c2c25
septalion <54902787+septalion@users.noreply.github.com>
2022-02-02 23:12:28 -0700

    RegistrationPAge+DataCollection

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,70 +1,65 @@
-import React from 'react';
-import {Image,TextInput,TouchableOpacity, ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {Image, ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
+import { TextInput,Form } from 'react-native';
+import Popup from '../components/Popup';
 
-function RegisterScreen({navigation}) {
+function VerifyFormFilled(Username,Password,Email,ConfirmPassword)
+{
+    console.log(Username)
+    console.log(Password)
+    console.log(Email)
+    console.log(ConfirmPassword)
+}
+export default function RegisterScreen({navigation}) {
+    const [Username ,SetUsername] = useState("Username");
+    const [Email ,SetEmail] = useState("Email");
+    const [Password ,SetPassword] = useState("Password");
+    const [ConfirmPassword ,SetConfirmPassword] = useState("Confirm Password");
     return (
-        <ImageBackground style={styles.background} source={require("../assets/BGs/background2.png")}>
-            
-            
-            <View style={styles.inputContainer}>
-         
-            <Text style={styles.createaccounttext}>Create Account </Text>
         
-          <TextInput style = {styles.input}
+        <ImageBackground style={styles.background} source={require("../assets/BGs/background2.png")}>
+         <Image style={styles.logo} source={require("../assets/Logo/logo1.png")}></Image>
+          <View style={styles.Signup} >
+           <TextInput style = {styles.input}
+                    onChangeText={(e) => SetUsername(e)}
+                    underlineColorAndroid = "transparent"
+                    placeholderTextColor = "#808080"
+                    autoCapitalize = "none"
+                    placeholder = "Username"
+               /> 
+               <TextInput style = {styles.input}
+               onChangeText={(e) => SetEmail(e)}
+                underlineColorAndroid = "transparent"
+                placeholder = "Email"
+                placeholderTextColor = "#808080"
+                autoCapitalize = "none"
+            />
+                    <TextInput style = {styles.input}
+                onChangeText={(e) => SetPassword(e)}                       
                underlineColorAndroid = "transparent"
-               placeholder = "   Username "
+               placeholder = "Password"
                placeholderTextColor = "#808080"
                autoCapitalize = "none"
                />
-               <TextInput style = {styles.input2}
+                <TextInput style = {styles.input}
+               onChangeText={(e) => SetConfirmPassword(e)}
                underlineColorAndroid = "transparent"
-               placeholder = "   Email"
+               placeholder = "Confirm Password"
                placeholderTextColor = "#808080"
                autoCapitalize = "none"
                />
-
-               <TextInput style = {styles.input3}
-               underlineColorAndroid = "transparent"
-               placeholder = "   Password"
-               placeholderTextColor = "#808080"
-               autoCapitalize = "none"
-               />
-               <TextInput style = {styles.input4}
-               underlineColorAndroid = "transparent"
-               placeholder = "   Re-enter Password"
-               placeholderTextColor = "#808080"
-               autoCapitalize = "none"
-               />
-
-</View>
-
-  
-           <View style={styles.googlelogoContainer}>
+               <Text onPress={VerifyFormFilled(Username,Password,Email,ConfirmPassword)} style = {styles.RegisterBtn}> Register </Text>
+               <Image style={styles.RegisterBtnBk} source={require("../assets/Btn/bluepillbutton.png")}></Image>
+                </View>
+                <View style={styles.googlelogoContainer}>
               <Image  style={styles.googlelogo} source={require("../assets/Btn/circlegoogle.png")}></Image>
               <Text style={styles.googleText}>Sign in with Google</Text>
            </View>
-           <Image style={styles.logo} source={require("../assets/Logo/logo1.png")}></Image>
-           
-           
-           
-           <View  style={styles.loginbuttonContainer}  >
-              <TouchableOpacity activeOpacity = { .6 } onPress ={() => navigation.navigate('SigninScreen')}>
-               <Text style={styles.logintext} >Sign-up</Text>
-                <Image style={styles.loginButton} source={require("../assets/Btn/bluepillbutton.png")}></Image>
-                
-             
-              {/* <Image onPress ={() => navigation.navigate('LandingScreen')} style={styles.loginButton} source={require("../assets/Btn/bluepillbutton.png")}></Image> */}
-              {/* <View style={styles.loginButton}></View> */}
-              
-              </TouchableOpacity>
+            <View  style={styles.trialButton}>
+                    
+                <Text  onPress ={() => navigation.navigate('SigninScreen')}>Click to go back to sign in </Text>
             </View>
-           
-           
-          
-          
-           
-           
-           
 
 
         </ImageBackground>
@@ -73,154 +68,80 @@ function RegisterScreen({navigation}) {
 }
 
 const styles = StyleSheet.create({
-    background: {
-    resizeMode:"contain", 
-    flex: 1,
-    justifyContent:"flex-end",
-    alignItems: "center",
-   
-  },
   logo: {
     flex: 1,
     width: 350,
     height: 100,
    
-    bottom: "40%"
+    bottom: "47%"
     
 
   },
-  signuptext:{
-    flex: 1,
-    
-    top:"50%",
-    fontSize:30,
-    color:"white",
-    fontWeight:"bold",
-    
-    
-
-  },
- 
-  input :{
-    
-    margin: 15,
-    width:330,
-    height: 40,
-    top:"5%",
-    borderColor: 'black',
-    borderWidth: 1,
-   
-    backgroundColor: "white",
-    borderRadius: 15,
-    
-  },
-  input2 :{
-    
-    margin: 15,
-    width:330,
-    height: 40,
-    top:"-10%",
-    borderColor: 'black',
-    borderWidth: 1,
-   
-    backgroundColor: "white",
-    borderRadius: 15,
-    
-  },
-  input3 :{
-    
-    margin: 15,
-    width:330,
-    height: 40,
-    top:"-25%",
-    borderColor: 'black',
-    borderWidth: 1,
-   
-    backgroundColor: "white",
-    borderRadius: 15,
-    
-  },
-  input4 :{
-    
-    margin: 15,
-    width:330,
-    height: 40,
-    top:"-40%",
-    borderColor: 'black',
-    borderWidth: 1,
-   
-    backgroundColor: "white",
-    borderRadius: 15,
-    
-  },
-  inputContainer:{
-   
-   flex: 1, 
-   top:"25%",
-    
-  },
-  loginButton:{
-    
-    
-   
-    
-  },
-  
-    logintext:{
-       zIndex: 999, // brings forward 
-       paddingLeft:"43%",
-       color:"white",
-       top:"45%",
-       fontSize: 20,
-       fontWeight:"bold",
-      
-    },
-    loginbuttonContainer:{
-      
-    
-      flex: 2,
-       top: "-15%",
-       
-      
-      
-    },
-
-
-    createaccounttext: {
-      zIndex: 999, // brings forward 
-      paddingLeft:"30%",
-      bottom:"-15%",
-      fontWeight:"bold",
-      color:"white",
-      fontSize:20,
-    },
-   
-    
-
-   
-    googlelogo: {
-      width: 40,
-      height: 40,
-     
-      bottom: 20,
-      
-     
-    },
-    googleText:{
-      fontSize:10,
-      top:"-5%",
-      fontWeight:"bold",
-     
-    },
-
-    googlelogoContainer: {
-      flex: 2,
-      top: "70%",
-      height: 20,
+    background: {
+        resizeMode:"contain", 
+      flex: 1,
+      justifyContent:"flex-end",
       alignItems: "center",
-       
+     
+    },
 
-    }
+    trialButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#fc5c65",
+      },
+      input :{
+      
+        margin: 15,
+        width:330,
+        height: 40,
+        top:"5%",
+        borderColor: 'black',
+        borderWidth: 1,
+       
+        backgroundColor: "white",
+        borderRadius: 15,
+        
+      },
+      Signup :{
+        
+        top:"3%",
+            flex: 1, 
+             
+           
+      }, googlelogo: {
+        width: 40,
+        height: 40,
+       
+        bottom: 20,
+        
+       
+      },
+      googleText:{
+        fontSize:10,
+        top:"-5%",
+        fontWeight:"bold",
+       
+      },
+
+      googlelogoContainer: {
+        flex: 2,
+        top: "35%",
+        height: 20,
+        alignItems: "center",
+         
+
+      },
+      RegisterBtn :{
+        zIndex: 999, // brings forward 
+        paddingLeft:"33%",
+        color:"white",
+        top:"25%",
+        fontSize: 20,
+        fontWeight:"bold",
+      },
+      RegisterBtnBk:{
+          top:"-10%"
+      }
 })
 
-export default RegisterScreen;
