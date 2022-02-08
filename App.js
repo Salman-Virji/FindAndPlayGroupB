630766371d4b29b81ac2dc78e706dbd85bbe1257
Jody <jarb88@hotmail.co.uk>
2022-02-08 19:41:24 +0000

    Updating files with comments - Jody

--- App.js
+++ App.js
@@ -1,31 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Pressable } from 'react-native';
-import SigninScreen from "./app/screens/SigninScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import LandingScreen from "./app/screens/LandingScreen";
-import ForgotPasswordScreen from './app/screens/ForgotPasswordScreen';
+import React from 'react'
+
+// Part of React Navigation to create navigation container
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// Importing screen components to Navigation Container
+import SigninScreen from './app/screens/SigninScreen';
+import RegisterScreen from './app/screens/RegisterScreen';
+import LandingScreen from './app/screens/LandingScreen';
+import ForgotPasswordScreen from './app/screens/ForgotPasswordScreen';
 
+// Part of React Navigation to create a 'Stack' style navigation object
 const Stack = createNativeStackNavigator();
 
-
-
 function App() {
-  return (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="SigninScreen" component={SigninScreen} options={{headerShown:false}} /> 
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown:false}} /> 
-      <Stack.Screen name="LandingScreen" component={LandingScreen} options={{headerShown:false}} /> 
-      <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} options={{headerShown:false}} /> 
-    </Stack.Navigator>
-
-
-    </NavigationContainer>
-   
- )
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='SigninScreen'
+                    component={SigninScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name='RegisterScreen'
+                    component={RegisterScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name='LandingScreen'
+                    component={LandingScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name='ForgotPasswordScreen'
+                    component={ForgotPasswordScreen}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 export default App;
-
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema(
+    {
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+    },
+    {
+        timstamps: true,
+    }
+);
 
-const userSchema = new Schema({
-    username:{type:String,required:true,unique:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
- },{
-     timstamps:true,
- });
-
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
