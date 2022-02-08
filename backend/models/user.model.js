f17bc2764153cac82b741fc39e581db4202d21bb
Jody <jarb88@hotmail.co.uk>
2022-02-08 20:01:46 +0000

    Updated some code and added comments

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
     {
         username: { type: String, required: true, unique: true },
         email: { type: String, required: true },
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,61 +1,70 @@
-const router = require('express').Router();
-const md5 = require('md5')
+// Split express and router into seperate variables for clarity.
+const express = require('express');
+const router = express.Router();
+
+// For salting passwords but we need to review this. Should be SHA-2 min.
+const md5 = require('md5');
+
 let User = require('../models/user.model');
 
+router.route('/').get((req, res) => {
+    res.send('user router');
+});
 
-router.route('/').get((req,res)=>{
-    res.send("user router")
-})
-router.route('/signup').post((req,res)=>{
+router.route('/signup').post((req, res) => {
     const username = req.body.username;
     const email = req.body.email;
-    var password = req.body.password;
+    let password = req.body.password; // let not var...
+
     password = md5(password);
 
-    const newUser= User({username,email,password});
+    const newUser = User({ username, email, password });
 
-    const data ={
-        msg:"",
-        status:false
-    }
+    const data = {
+        msg: '',
+        status: false,
+    };
 
-    newUser.save()
-        .then(()=>{
-            data.msg = "User Added!";
+    newUser
+        .save()
+        .then(() => {
+            data.msg = 'User Added!';
             data.status = true;
-            res.json(data)
+            res.json(data);
         })
-        .catch(err=>{
-            data.msg="Error: "+err
+        .catch((err) => {
+            data.msg = 'Error: ' + err;
             data.status = false;
-            res.send(data)
+            res.send(data);
         });
-})
+});
 
-router.route('/login').post((req,res)=>{
+router.route('/login').post((req, res) => {
+    const data = {
+        msg: '',
+        status: false,
+    };
 
-    const data ={
-        msg:"",
-        status:false
-    }
+    User.find({ username: req.body.username })
+        .then((user) => {
+            if (req.body.user == '' || req.body.password == '') {
+                data.msg = 'Please Complete All Fields';
+                res.send(data);
+                return;
+            }
+            if (user[0].password == md5(req.body.password)) {
+                data.msg = '';
+                data.status = true;
+                res.send(data);
+            } else {
+                data.msg = 'Wrong Password';
+                data.status = false;
+                res.send(data);
+            }
+        })
+        .catch((err) => {
+            console.log('No user found');
+        });
+});
 
-    User.find({username:req.body.username}).then(user=>{
-
-        if(req.body.user == '' || req.body.password == ''){
-            data.msg="Please Complete All Fields"
-            res.send(data)
-            return;
-        }
-        if(user[0].password == md5(req.body.password)){
-            data.msg=""
-            data.status=true
-            res.send(data)
-        }else{
-            data.msg="Wrong Password"
-            data.status=false
-            res.send(data)
-        }
-    }).catch(err=>{console.log("No user found")})
-
-})
 module.exports = router;
