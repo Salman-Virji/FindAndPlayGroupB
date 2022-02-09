ec6370f57e43abc2c4524df45cc98896c45f257f
faizan <faizanshafiq123@gmail.com>
2022-02-08 21:33:35 -0700

    Faizan Created a new screen SigninScreenT for tablets.

--- App.js
+++ App.js
@@ -1,31 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Pressable } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import SigninScreen from "./app/screens/SigninScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LandingScreen from "./app/screens/LandingScreen";
-import ForgotPasswordScreen from './app/screens/ForgotPasswordScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import ForgotPasswordScreen from "./app/screens/ForgotPasswordScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SigninScreenT from "./app/screens/SigninScreenT";
 
 const Stack = createNativeStackNavigator();
 
-
-
 function App() {
   return (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="SigninScreen" component={SigninScreen} options={{headerShown:false}} /> 
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown:false}} /> 
-      <Stack.Screen name="LandingScreen" component={LandingScreen} options={{headerShown:false}} /> 
-      <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} options={{headerShown:false}} /> 
-    </Stack.Navigator>
-
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SigninScreenT"
+          component={SigninScreenT}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SigninScreen"
+          component={SigninScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LandingScreen"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ForgotPasswordScreen"
+          component={ForgotPasswordScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
-   
- )
+  );
 }
 export default App;
-
