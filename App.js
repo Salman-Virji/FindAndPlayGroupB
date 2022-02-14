ac271519729f1b505bafbcedde312ae7d78ea75c
gurpreet19-art <75091414+gurpreet19-art@users.noreply.github.com>
2022-02-14 13:36:25 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,56 +1,261 @@
-import React from 'react'
+import React from 'react';
+import { Button,InputEvent 
+  ,Image, ImageBackground,
+   StyleSheet, Text,
+    TouchableOpacity, 
+    View, TextInput } from 'react-native';
 
-// Part of React Navigation to create navigation container
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+function CreateGame(props) {
+    return (
+     <View >
 
-// Importing screen components to Navigation Container
-import SigninScreen from './app/screens/SigninScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-import LandingScreen from './app/screens/LandingScreen';
-import ForgotPasswordScreen from './app/screens/ForgotPasswordScreen';
-import SigninScreenT from "./app/screens/SigninScreenT";
-import ForgotPasswordTablet from "./app/screens/ForgotPasswordTablet";
 
-// Part of React Navigation to create a 'Stack' style navigation object
-const Stack = createNativeStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SigninScreenT"
-          component={SigninScreenT}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ForgotPasswordTablet"
-          component={ForgotPasswordTablet}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SigninScreen"
-          component={SigninScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="RegisterScreen"
-          component={RegisterScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="LandingScreen"
-          component={LandingScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ForgotPasswordScreen"
-          component={ForgotPasswordScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+
+<Image source={require('./assets/Settings.png')}  style ={{
+  width:150,
+  height:150,
+position:'absolute',
+top:80,
+right:50
 }
-export default App;
+  
+}/>
+
+
+<Image source={require('./assets/Logout.png')}  style ={{
+  width:150,
+  height:150,
+position:'absolute',
+top:80,
+left:50
+}
+}/>
+
+
+<View >
+  <Text style={{
+  fontSize:60,
+  fontWeight: 'bold',
+  position:'absolute',
+  top:220,
+  left:220
+
+}}>Create Game</Text>
+</View>
+
+
+<Image source={require('./assets/Profile.png')}  style ={{
+  width:150,
+  height:150,
+position:'absolute',
+top:1000,
+left:50
+}
+}/>
+
+<Image source={require('./assets/Play.png')}  style ={{
+  width:150,
+  height:150,
+position:'absolute',
+top:1000,
+right:330
+}
+}/>
+
+<Image source={require('./assets/Feed.png')}  style ={{
+  width:150,
+  height:150,
+position:'absolute',
+top:1000,
+right:50
+}
+}/>
+
+
+
+
+<TextInput 
+               underlineColorAndroid = "transparent"
+               placeholder = "Title"
+               placeholderTextColor = "#808080"
+               autoCapitalize = "none"
+               style={{
+                 position:'absolute',
+                 top:330,
+                 borderRightWidth: 2,
+                 borderLeftWidth: 2,
+                 borderTopWidth:2,
+                 borderBottomWidth:2,
+                 width: 350,
+                 alignItems:'center',
+                 textAlign:'center',
+                 padding:12,
+                 left: 220,
+                 fontSize:30,
+                 borderRadius:20
+
+
+               }}
+               />
+
+
+
+               
+<TextInput 
+               underlineColorAndroid = "transparent"
+               placeholder = "Category"
+               placeholderTextColor = "#808080"
+               autoCapitalize = "none"
+               style={{
+                 position:'absolute',
+                 top:440,
+                 borderRightWidth: 2,
+                 borderLeftWidth: 2,
+                 borderTopWidth:2,
+                 borderBottomWidth:2,
+                 width: 350,
+                 alignItems:'center',
+                 textAlign:'center',
+                 padding:12,
+                 left: 220,
+                 fontSize:30,
+                 borderRadius:20
+
+
+               }}
+               />
+
+
+               
+<TextInput 
+               underlineColorAndroid = "transparent"
+               placeholder = "Time Limit"
+               placeholderTextColor = "#808080"
+               autoCapitalize = "none"
+               style={{
+                 position:'absolute',
+                 top:540,
+                 borderRightWidth: 2,
+                 borderLeftWidth: 2,
+                 borderTopWidth:2,
+                 borderBottomWidth:2,
+                 width: 350,
+                 alignItems:'center',
+                 textAlign:'center',
+                 padding:12,
+                 left: 220,
+                 fontSize:30,
+                 borderRadius:20
+
+
+               }}
+               />
+
+               
+
+            
+<View style={{
+  position:'absolute',
+  top:700,
+  left:250,
+  borderColor:'black',
+    width:300,
+
+
+ zIndex:3,
+fontSize:20,
+
+}} >
+               
+                <TouchableOpacity activeOpacity={0.95} style={styles.button}>
+                    <Text style={styles.text}>HOST</Text>
+                </TouchableOpacity>
+            </View>
+
+
+
+
+
+
+
+
+
+
+<TextInput 
+               underlineColorAndroid = "transparent"
+               placeholder = "Difficulty"
+               placeholderTextColor = "#808080"
+               autoCapitalize = "none"
+               style={{
+                 position:'absolute',
+                 top:640,
+                 borderRightWidth: 2,
+                 borderLeftWidth: 2,
+                 borderTopWidth:2,
+                 borderBottomWidth:2,
+                 width: 350,
+                 alignItems:'center',
+                 textAlign:'center',
+                 padding:12,
+                 left: 220,
+                 fontSize:30,
+                 borderRadius:20
+
+
+               }}
+               />
+
+
+    
+     
+     </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    background:{
+
+       position:'absolute',
+       top:200
+
+
+    },
+    texts:{
+      fontSize:30
+
+    },
+   
+  button: {
+      flexDirection: 'row', 
+      height: 50, 
+      backgroundColor: 'lightblue',
+      borderRadius:20,
+    
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 50,
+      elevation:3,
+  },
+  button2: {
+    flexDirection: 'row', 
+    height: 50, 
+    backgroundColor: 'lightblue',
+    borderRadius:20,
+
+   
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    elevation:3,
+},
+  text: {
+      fontSize: 20,
+      padding:10,
+      
+      fontWeight: 'bold',
+  }
+})
+
+export default CreateGame;
