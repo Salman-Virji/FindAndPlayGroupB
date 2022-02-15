67eddb3d78953b07efb7cc2c996e03012e4e00ed
faizan <faizanshafiq123@gmail.com>
2022-02-15 15:34:13 -0700

    Mergin tablet design into SignInScreen.js and  SignInScreenT.js file for consistensy.

--- app/screens/SigninScreen.js
+++ app/screens/SigninScreen.js
@@ -1,270 +1,365 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+//Installing expo checkbox as react native doesn't provide checkbox out of the box anymore.
+import Checkbox from "expo-checkbox";
+import React, { useState } from "react";
+import axios from "axios";
+
+//Importing whatever components are required from react native
 import {
-    TextInput,
-    Image,
-    ImageBackground,
-    Pressable,
-    StyleSheet,
-    Text,
-    View,
-    TouchableHighlight,
-    TouchableWithoutFeedback,
-    TouchableOpacity,
-    KeyboardAvoidingView,
-} from 'react-native';
+  TextInput,
+  Image,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
+
+//This component is used to calculate the dimensions of the device and set width of certain components accordingly e.g input box
+import { Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
 
 function SigninScreen({ navigation }) {
-    const [username, setUsername] = useState('user');
-    const [password, setPassword] = useState('pass');
-    const [message, setMessage] = useState('');
-
+    const [username, setUsername] = useState("user");
+    const [password, setPassword] = useState("pass");
+    const [message, setMessage] = useState("");
+  
     function clearFields() {
-        setUsername('');
-        setPassword('');
+      setUsername("");
+      setPassword("");
     }
-
+  
     function Login() {
-        const body = {
-            username: username,
-            password: password,
-        };
-
-        var url = `http://10.0.0.180:3000/users/login`; //Replace by your IP address
-
-        axios
-            .post(url, body, navigation)
-            .then((res) => {
-                console.log(res.data);
-                clearFields();
-                setMessage(res.data.msg);
-
-                if (res.data.status == true) {
-                    navigation.navigate('LandingScreen', res.data);
-                }
-            })
-            .catch((err) => console.log('error'));
+      const body = {
+        username: username,
+        password: password,
+      };
+  
+      var url = `http://10.0.0.180:3000/users/login`; //Replace by your IP address
+  
+      axios
+        .post(url, body, navigation)
+        .then((res) => {
+          console.log(res.data);
+          clearFields();
+          setMessage(res.data.msg);
+  
+          if (res.data.status == true) {
+            navigation.navigate("LandingScreen", res.data);
+          }
+        })
+        .catch((err) => console.log("error"));
     }
-
+  
     function SignUp() {
-        const body = {
-            username: username,
-            password: password,
-        };
-        if (username != '' || password != '') {
-            var url = `http://10.0.0.180:3000/users/signup`; //Replace by your IP address
-            axios
-                .post(url, body)
-                .then(() => {
-                    clearFields();
-                    setMessage('Username is taken');
-                })
-                .catch((err) => console.log('err'));
-        } else {
-            console.log('Empty space');
-        }
+      const body = {
+        username: username,
+        password: password,
+      };
+      if (username != "" || password != "") {
+        var url = `http://10.0.0.180:3000/users/signup`; //Replace by your IP address
+        axios
+          .post(url, body)
+          .then(() => {
+            clearFields();
+            setMessage("Username is taken");
+          })
+          .catch((err) => console.log("err"));
+      } else {
+        console.log("Empty space");
+      }
     }
-
-    return (
-        <ImageBackground
-            style={styles.background}
-            source={require('../assets/BGs/background1.png')}
+  
+  return (
+    //Setting background
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/BGs/background2.png")}
+    >
+      {/* //View is like a div in html */}
+      <View style={styles.inputContainer}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            width: "100%",
+            margin: 100,
+          }}
         >
-            <View style={styles.inputContainer}></View>
-            <Text style={styles.signintext}> Sign-In </Text>
+          <Image
+            style={styles.logo}
+            source={require("../assets/Logo/logo1.png")}
+          />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "flex-end",
+          }}
+        >
+          {/* To use text we have to use Text component */}
+          <Text style={styles.signintext}> Sign-In </Text>
+
+          {/* TextInput is like input box */}
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="   Username or Email"
+            placeholderTextColor="#fff"
+            autoCapitalize="none"
+            value={username}
+        onChangeText={(e) => setUsername(e)}
+          />
+          <TextInput
+            style={styles.input2}
+            underlineColorAndroid="transparent"
+            placeholder="   Password"
+            placeholderTextColor="#fff"
+            autoCapitalize="none"
+            value={password}
+            onChangeText={(e) => setPassword(e)}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            width: width * 0.6,
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <Checkbox
+              // style={styles.checkbox}
+              value={true}
+              // onValueChange={setChecked}
+              color={true ? "#4630EB" : undefined}
+            />
+            <Text style={{ color: "white" }}>Remember me!</Text>
+          </View>
+          <View style={{}}>
             <Text
-                style={styles.forgotpasstext}
-                onPress={() => navigation.navigate('ForgotPasswordScreen')}
+              style={{
+                alignItems: "center",
+                color: "white",
+                fontWeight: "bold",
+              }}
+              onPress={() => navigation.navigate("ForgotPasswordTablet")}
             >
-                {' '}
-                Forgot Password ?{' '}
+              Forgot Password ?{" "}
             </Text>
+          </View>
+        </View>
+        <View style={styles.loginbuttonContainer}>
+          <View>
+            {/* Pressable makes the area Pressable */}
+            <Pressable
+              style={styles.loginButton}
+              onPress={() => navigation.navigate("LandingScreen")} //changed onclick to go to landingscreen
+            >
+              <Text style={styles.loginText}>Login</Text>
+            </Pressable>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Pressable
+              style={styles.btnWithoutAccount}
+              onPress={() => navigation.navigate("LandingScreen")} //changed onclick to go to landingscreen
+            >
+              <Text style={([styles.loginText], { color: "black" })}>
+                Continue without account
+              </Text>
+            </Pressable>
+          </View>
+        </View>
 
-            <TextInput
-                style={styles.input}
-                underlineColorAndroid='transparent'
-                placeholder='   Username or Email'
-                placeholderTextColor='#808080'
-                autoCapitalize='none'
-                value={username}
-                onChangeText={(e) => setUsername(e)}
-            />
-            <TextInput
-                style={styles.input2}
-                underlineColorAndroid='transparent'
-                placeholder='   Password'
-                placeholderTextColor='#808080'
-                autoCapitalize='none'
-                value={password}
-                onChangeText={(e) => setPassword(e)}
-            />
+        <View style={styles.signupbuttonContainer}>
+          <Text style={{ bottom: 10, fontWeight: "bold", fontSize: 20 }}>
+            Not registered?
+          </Text>
 
-            <View style={styles.googlelogoContainer}>
-                <Image
-                    style={styles.googlelogo}
-                    source={require('../assets/Btn/circlegoogle.png')}
-                ></Image>
-                <Text style={styles.googleText}>Sign in with Google</Text>
-            </View>
-            <Image
-                style={styles.logo}
-                source={require('../assets/Logo/logo1.png')}
-            ></Image>
-
-            <View style={styles.loginbuttonContainer}>
-                <TouchableOpacity activeOpacity={0.6} onPress={Login}>
-                    {/*onPress ={() => navigation.navigate('LandingScreen')} */}
-
-                    <Text style={styles.logintext}>Log-In</Text>
-                    <Image
-                        style={styles.loginButton}
-                        source={require('../assets/Btn/bluepillbutton.png')}
-                    ></Image>
-
-                    {/* <Image onPress ={() => navigation.navigate('LandingScreen')} style={styles.loginButton} source={require("../assets/Btn/bluepillbutton.png")}></Image> */}
-                    {/* <View style={styles.loginButton}></View> */}
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.signupbuttonContainer}>
-                <TouchableOpacity
-                    activeOpacity={0.6}
-                    onPress={() => navigation.navigate('RegisterScreen')}
-                >
-                    <Image
-                        style={styles.signupButton}
-                        source={require('../assets/Btn/orangepillbutton.png')}
-                    ></Image>
-                    <Text style={styles.notregisteredtext}>
-                        {' '}
-                        Not Registered ?{' '}
-                    </Text>
-                    <Text style={styles.signuptext}>Sign up </Text>
-                </TouchableOpacity>
-            </View>
-        </ImageBackground>
-    );
+          <Pressable
+            style={styles.btnSignup}
+            onPress={() => console.log("Pressed")}
+          >
+            <Text
+              style={([styles.loginText], { fontSize: 15 })}
+              onPress={() => navigation.navigate("RegisterScreen")}
+            >
+              Continue without account
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        resizeMode: 'contain',
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-    },
-    logo: {
-        flex: 1,
-        width: 350,
-        height: 100,
+  background: {
+    resizeMode: "contain",
+    flex: 1,
+  },
+  logo: {
+    top: "10%",
+    width: "100%",
+    height: "100%",
+  },
+  signintext: {
+    fontSize: 50,
 
-        bottom: '47%',
-    },
-    signintext: {
-        flex: 1,
-        zIndex: 999,
-        top: '15%',
-        fontSize: 30,
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    forgotpasstext: {
-        flex: 1,
-        zIndex: 999,
-        paddingTop: 0,
-        paddingLeft: 190,
-        top: '28%',
-        fontSize: 10,
-        color: 'black',
-        fontWeight: 'bold',
-    },
-    input: {
-        margin: 15,
-        width: 330,
-        height: 40,
-        top: '5%',
-        borderColor: 'black',
-        borderWidth: 1,
+    color: "white",
+    fontWeight: "bold",
+  },
+  forgotpasstext: {
+    flex: 1,
+    paddingTop: 0,
+    paddingLeft: 190,
+    fontSize: 10,
+    color: "black",
+    fontWeight: "bold",
+  },
+  input: {
+    marginBottom: 15,
+    width: width * 0.6,
+    height: 50,
+    borderColor: "#fff",
+    borderWidth: 3,
+    color: "#fff",
+    borderRadius: 20,
+    fontSize: 20,
+    paddingLeft: 15,
+  },
+  input2: {
+    marginBottom: 15,
+    width: width * 0.6,
+    height: 50,
+    borderColor: "#fff",
+    borderWidth: 3,
+    color: "#fff",
+    borderRadius: 20,
+    fontSize: 20,
+    paddingLeft: 15,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  loginButton: {},
 
-        backgroundColor: 'white',
-        borderRadius: 15,
-    },
-    input2: {
-        margin: 15,
-        width: 330,
-        height: 40,
-        top: '2%',
-        borderColor: 'black',
-        borderWidth: 1,
+  logintext: {
+    zIndex: 999, // brings forward
+    paddingLeft: "43%",
+    color: "white",
+    top: "45%",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  loginbuttonContainer: {
+    flex: 1,
+    width: width * 0.6,
+    justifyContent: "center",
+  },
 
-        backgroundColor: 'white',
-        borderRadius: 15,
-    },
-    inputContainer: {
-        flex: 1,
-    },
-    loginButton: {},
+  signuptext: {
+    zIndex: 999, // brings forward
 
-    logintext: {
-        zIndex: 999, // brings forward
-        paddingLeft: '43%',
-        color: 'white',
-        top: '45%',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    loginbuttonContainer: {
-        flex: 2,
-        top: '-23%',
-    },
+    bottom: "58%",
+    paddingLeft: "41%",
+    fontWeight: "bold",
+    color: "white",
+  },
+  notregisteredtext: {
+    color: "black",
 
-    signuptext: {
-        zIndex: 999, // brings forward
+    paddingLeft: "35%",
+    bottom: "63%",
+    fontSize: 12,
+  },
+  signupbuttonContainer: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    width: 400,
+    justifyContent: "center",
+  },
 
-        bottom: '58%',
-        paddingLeft: '41%',
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    notregisteredtext: {
-        color: 'black',
+  trialButton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#fc5c65",
+  },
 
-        paddingLeft: '35%',
-        bottom: '63%',
-        fontSize: 12,
-    },
-    signupbuttonContainer: {
-        flex: 2,
+  googlelogo: {
+    width: 40,
+    height: 40,
 
-        alignItems: 'center',
-        top: '-3%',
-        width: 400,
-    },
+    bottom: 20,
+  },
+  googleText: {
+    fontSize: 10,
+    top: "-15%",
+    fontWeight: "bold",
+  },
 
-    trialButton: {
-        width: 100,
-        height: 50,
-        backgroundColor: '#fc5c65',
-    },
+  googlelogoContainer: {
+    flex: 2,
+    top: "30%",
+    height: 20,
+    alignItems: "center",
+  },
+  loginButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 30,
+    elevation: 5,
+    backgroundColor: "#50A4FF",
+    width: "100%",
+    height: 60,
+    shadowColor: "rgba(46, 229, 157, 0.4)",
+  },
+  loginText: {
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  btnSignup: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 30,
+    elevation: 5,
+    backgroundColor: "#FF6551",
+    width: "70%",
+    fontSize: 20,
+    height: 60,
+    shadowColor: "rgba(46, 229, 157, 0.4)",
+    fontSize: 20,
+  },
+  btnWithoutAccount: {
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 30,
+    elevation: 5,
+    backgroundColor: "#FFE551",
+    width: "70%",
+    height: 60,
 
-    googlelogo: {
-        width: 40,
-        height: 40,
-
-        bottom: 20,
-    },
-    googleText: {
-        fontSize: 10,
-        top: '-15%',
-        fontWeight: 'bold',
-    },
-
-    googlelogoContainer: {
-        flex: 2,
-        top: '30%',
-        height: 20,
-        alignItems: 'center',
-    },
+    shadowColor: "rgba(46, 229, 157, 0.4)",
+  },
 });
 
 export default SigninScreen;
