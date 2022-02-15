a798264246b056dc9644a95972b956f02788928a
Jody <jarb88@hotmail.co.uk>
2022-02-15 20:32:52 +0000

    fix user.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,66 +19,6 @@ router.route('/signup').post((req, res) => {
     let password = req.body.password; // let not var...
 
     password = md5(password);
-const router = require('express').Router();
-//For salting passwords
-//const md5 = require('md5');
-let User = require('../models/User');
-
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const session = require('express-session');
-const flash = require('express-flash');
-
-//router.use(express.urlencoded({extended:false}))
-
-router.use(session({
-    secret: "secretKey123",
-    resave: false,
-    saveUninitialized: false
-}));
-
-router.use(flash());
-router.use(passport.initialize());
-router.use(passport.session());
-
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        done(err, user);
-    })
-});
-
-passport.use(new LocalStrategy(async function (username, password, done) {
-    User.findOne({username: username}, function (err, user) {
-        if(err) return done(err);
-        if(!user) return done(null, false, {message: 'Invalid username!'});
-
-        bcrypt.compare(password, user.password, async function (err, res) {
-            if(err) return done(err);
-            if(res === false) return done(null, false, {message: 'Invalid password!'});
-
-            //userLoggedInUsername = User.findOne({username: username})
-            console.log(user);
-            return done(null, user);
-        })
-    })
-}));
-
-router.route('/').get((req, res) => {
-    res.send("user router");
-});
-
-//User signup
-router.route('/signup').post(async(req, res) => {
-    //Creating variables
-    const SafePass = await bcrypt.hash(req.body.password, 10);
-    const username = req.body.username;
-    const email = req.body.email;
-    let password = SafePass;
 
     const newUser = User({ username, email, password });
 
@@ -102,24 +42,6 @@ router.route('/signup').post(async(req, res) => {
 });
 
 router.route('/login').post((req, res) => {
-//User login
-router.route('/login').get((req, res) => {
-    console.log('Failed');
-});
-
-router.route('/success').get((req,res) => {
-    console.log('Success')
-    const data = {
-        msg: "",
-        status: false
-    }
-    data.msg = "Valid password";
-    data.status = true;
-    res.send(data);
-});
-
-router.route('/login').post((req,res) => {
-    console.log('login')
     const data = {
         msg: '',
         status: false,
@@ -145,39 +67,6 @@ router.route('/login').post((req,res) => {
         .catch((err) => {
             console.log('User not found');
         });
-        //If fields are empty, the user will be asked to complete them
-        if (req.body.user == '' || req.body.password == '') {
-            data.msg = "Please complete all fields";
-            res.send(data);
-            return;
-        }
-        //If the password entered by the user and the password stored in the database are the same, user will be logged in
-        let userPassword = req.body.password;
-        passwordTrial = false
-
-        bcrypt.compare(userPassword, user.password, (req, res) =>{
-            validLogin();
-        })
-
-        function validLogin() {
-            data.msg = "Valid password";
-            data.status = true;
-            res.send(data);
-        }
-
-        if (passwordTrial) {
-            console.log('Valid password')
-            data.msg = "Valid password";
-            data.status = true;
-            res.send(data);
-        } else {
-            data.msg = "Invalid password";
-            data.status = false;
-            res.send(data);
-        }
-    }).catch(err => {
-        console.log("User not found");
-    });
 });
 
 module.exports = router;
