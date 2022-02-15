c65020bd6e20f62b15c048dc71f0985051673d84
faizan <faizanshafiq123@gmail.com>
2022-02-15 15:42:30 -0700

    Remove SignInScreenT.js from app.js

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 // Part of React Navigation to create navigation container
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Importing screen components to Navigation Container
-import SigninScreen from './app/screens/SigninScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-import LandingScreen from './app/screens/LandingScreen';
-import ForgotPasswordScreen from './app/screens/ForgotPasswordScreen';
-import SigninScreenT from "./app/screens/SigninScreenT";
+import SigninScreen from "./app/screens/SigninScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
+import LandingScreen from "./app/screens/LandingScreen";
+import ForgotPasswordScreen from "./app/screens/ForgotPasswordScreen";
+
 import ForgotPasswordTablet from "./app/screens/ForgotPasswordTablet";
 
 // Part of React Navigation to create a 'Stack' style navigation object
@@ -27,11 +27,6 @@ function App() {
         <Stack.Screen
           name="ForgotPasswordTablet"
           component={ForgotPasswordTablet}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SigninScreenT" //For testing, change from SigninScreen to SigninScreenT
-          component={SigninScreenT} //For testing, change from SigninScreen to SigninScreenT
           options={{ headerShown: false }}
         />
         <Stack.Screen
