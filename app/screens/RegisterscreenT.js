2706976f66f3a4823406cb206763a18d3b533559
septalion <54902787+septalion@users.noreply.github.com>
2022-02-17 11:24:10 -0700

    CleanedupCode/Comments

--- app/screens/RegisterscreenT.js
+++ app/screens/RegisterscreenT.js
@@ -84,6 +84,7 @@ export default function RegisterScreenT({ navigation }) {
   const [Email, SetEmail] = useState("Email");
   const [Password, SetPassword] = useState("Password");
   const [ConfirmPassword, SetConfirmPassword] = useState("Confirm Password");
+  // uses state to track which msg's should be displayed and update the page dynamically
   const [usernameV,UpdateUsernameVisbility]= useState(false);
   const [emailV,UpdateEmailVisbility]= useState(false);
   const [passwordV,UpdatePassVisbility]= useState(false);
@@ -93,10 +94,6 @@ export default function RegisterScreenT({ navigation }) {
       style={styles.background}
       source={require("../assets/BGs/background2.png")}
     >
-      {/* <Image
-            style={styles.logo}
-            source={require("../assets/Logo/logo1.png")}
-          /> */}
       <Text style={styles.logo}> Find & Play</Text>
 
       <View
@@ -112,8 +109,9 @@ export default function RegisterScreenT({ navigation }) {
       >
         <Text style={styles.SignupTxT}> Sign up</Text>
       
-        {// controlls if the text is visible or not
-         usernameV ? <Text style={styles.inputTxt}>Please enter a username</Text> :<Text style={styles.inputTxt}></Text>}
+        {//inoput fields and disdplay mesages for if form is not completed properly
+        // controlls if the text is visible or not
+         usernameV ? <Text style={styles.input, styles.inputTxt}>Please enter a username</Text> :<Text style={styles.input, styles.inputTxt}></Text>}
         <TextInput
           style={styles.input}
           onChangeText={(e) => SetUsername(e)}
@@ -123,7 +121,7 @@ export default function RegisterScreenT({ navigation }) {
           placeholder="Username"
           placeholderTextColor="#fff"
         />
-        { emailV ? <Text style={styles.inputTxt}>Please enter a Valid Email</Text> : <Text style={styles.inputTxt}></Text>}
+        { emailV ? <Text style={styles.input, styles.inputTxt}>Please enter a Valid Email</Text> : <Text style={styles.input, styles.inputTxt}></Text>}
         
         <TextInput
           style={styles.input}
@@ -133,7 +131,7 @@ export default function RegisterScreenT({ navigation }) {
           autoCapitalize="none"
           placeholderTextColor="#fff"
         />
-        { passwordV ? <Text style={styles.inputTxt}>Please enter a Password</Text> : <Text style={styles.inputTxt}></Text>}
+        { passwordV ? <Text style={styles.input, styles.inputTxt}>Please enter a Password</Text> : <Text style={styles.input, styles.inputTxt}></Text>}
         <TextInput
           style={styles.input}
           onChangeText={(e) => SetPassword(e)}
@@ -143,7 +141,7 @@ export default function RegisterScreenT({ navigation }) {
           placeholderTextColor="#fff"
           secureTextEntry={true}
         />
-        { CpasswordV ? <Text style={styles.inputTxtP}>Please make sure your passwords match</Text> : <Text style={styles.inputTxt}></Text>}
+        { CpasswordV ? <Text style={styles.input, styles.inputTxtP}>Please make sure your passwords match</Text> : <Text style={styles.input,  styles.inputTxtP}></Text>}
         <TextInput
           style={styles.input}
           onChangeText={(e) => SetConfirmPassword(e)}
@@ -210,23 +208,6 @@ export default function RegisterScreenT({ navigation }) {
 
 const styles = StyleSheet.create({
 
-  HomeBtn: {
-    position: "relative",
-    padding: 7,
-    paddingLeft: 25,
-    left: "10%",
-    color: "white",
-    fontSize: 35,
-    fontWeight: "bold",
-    backgroundColor: "#FF6551",
-    borderRadius: 50,
-    width: "20%",
-  },
-  HomeView: {
-    position: "relative",
-    left: "18%",
-    top: "15%",
-  },
   loginbuttonContainer: {
     flex: 1,
     flexDirection: "column",
@@ -284,34 +265,24 @@ const styles = StyleSheet.create({
   },
   inputTxt: {
     color:"white",
-              textShadowColor: "rgba(0, 0, 0, 1)",
-              textShadowOffset: { width: -1, height:1 },
-              textShadowRadius: 10,
-    left: "20%",
+    textShadowColor: "rgba(0, 0, 0, 1)",
+    textShadowOffset: { width: -1, height:1 },
+    textShadowRadius: 10,
+    left: "21%",
     top: "-10%",
-    width: width * 0.6,
     fontSize: 20,
     fontWeight: "bold",
-    paddingLeft: 17,
-    paddingRight: 15,
-    position: "relative",
   },
   inputTxtP: {
-    fontWeight: "bold",
-      color:"white",
-                textShadowColor: "rgba(0, 0, 0, 1)",
-                textShadowOffset: { width: -1, height:1 },
-                textShadowRadius: 10,
     left: "13%",
     top: "-10%",
-    width: width * 0.6,
+    color:"white",
+    textShadowColor: "rgba(0, 0, 0, 1)",
+    textShadowOffset: { width: -1, height:1 },
+    textShadowRadius: 10,
     fontSize: 20,
-    paddingLeft: 17,
-    paddingRight: 15,
-    position: "relative",
+    fontWeight: "bold",
   },
-
-
   registerbuttonContainer: {
     position:"relative",
     width: width * 0.6,
