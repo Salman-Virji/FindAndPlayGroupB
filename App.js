494d7c9c7f6fd921208a861ffa4c201862f757b5
JodyB <jarb88@hotmail.co.uk>
2022-03-21 20:17:41 +0000

    Moved App Copy to Backend

--- App.js
+++ App.js
@@ -1,51 +1,74 @@
-import React from 'react';
+import React from "react";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// Part of React Navigation to create navigation container
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import * as Group2B from './app/screens';
-
+// Importing screen components to Navigation Container
+import Lobby from "./backend/testing/app_testing/screens/InApp/GameLobby";
+import SigninScreen from "../app/screens/SigninScreen";
+import RegisterScreen from "../app/screens/RegisterScreen";
+import RegistarScreenT from "../app/screens/RegisterscreenT";
+import LandingScreen from "./backend/testing/app_testing/screens/InApp/LandingScreen";
+import JoinGame from "./backend/testing/app_testing/screens/InApp/JoinGameScreen";
+import ForgotPasswordScreen from "../app/screens/ForgotPasswordScreen";
+import ForgotPasswordTablet from "../app/screens/ForgotPasswordTablet";
+import CreateGameScreen from "./backend/testing/app_testing/screens/InApp/CreateGameScreen";
+// Part of React Navigation to create a 'Stack' style navigation object
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-    const [validToken, setValidToken] = React.useState(false);
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                {validToken ? (
-                    <>
-                        {/* PROTECTED */}
-                        <Stack.Screen
-                            name='Protected'
-                            component={Group2B.ProtectedScreen}
-                            options={{ headerShown: false }}
-                        />
-                    </>
-                ) : (
-                    <>
-                        {/* SIGN IN */}
-                        <Stack.Screen
-                            name='SignIn'
-                            component={Group2B.SignInScreen}
-                            options={{ headerShown: false }}
-                        />
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="LandingScreen"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SigninScreen"
+          component={SigninScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ForgotPasswordTablet"
+          component={ForgotPasswordTablet}
+          options={{ headerShown: false }}
+        />
 
-                        {/* SIGN UP */}
-                        <Stack.Screen
-                            name='SignUp'
-                            component={Group2B.SignUpScreen}
-                            options={{ headerShown: false }}
-                        />
-
-                        {/* RESET PASSWORD */}
-                        <Stack.Screen
-                            name='ResetPassword'
-                            component={Group2B.ResetPasswordScreen}
-                            options={{ headerShown: false }}
-                        />
-                    </>
-                )}
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RegistarScreenT"
+          component={RegistarScreenT}
+          options={{ headerShown: false }}
+        />
+        
+        <Stack.Screen
+          name="ForgotPasswordScreen"
+          component={ForgotPasswordScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="CreateGameScreen"
+          component={CreateGameScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="JoinGame"
+          component={JoinGame}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="gamelobby"
+          component={Lobby}
+          options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+export default App;
