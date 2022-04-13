a53a9da15f3e53ddc237082d24f2cf3c42f1cad5
Salman Virji <salmanvirji@gmail.com>
2022-04-13 17:42:41 -0600

    Faizans -Created a method for getting random facts in Landing Screen.

--- app/screens/LandingScreen.js
+++ app/screens/LandingScreen.js
@@ -11,11 +11,18 @@ import {
   ScrollView,
 } from "react-native";
 import FunFact from "./FunFactsComponent";
+import imageArray from "../assets/images.js";
 
-function LandingScreen({ route ,navigation }) {
+function LandingScreen({ route, navigation }) {
   const [Isvisible, setVisibility] = useState(false);
   const GameLobby = route.params.GameLobby;
- // console.log("lobby at landing ", GameLobby , "\n Objectives array :", GameLobby.objectives);
+  const { newImages } = imageArray();
+  console.log(
+    "lobby at landing ",
+    GameLobby,
+    "\n Objectives array :",
+    GameLobby.objectives
+  );
   // -- useState variables
   //   const [img, setImg] = useState({Value:" ",Index:0});
   //   var obj =[];
@@ -46,8 +53,6 @@ function LandingScreen({ route ,navigation }) {
       style={styles.background}
       source={require("../assets/BGs/background1.png")}
     >
-   
-
       {/*FACTS banner*/}
       <View style={styles.funFactContainer}>
         <Text style={styles.funFactContainerText}>Fun Facts</Text>
@@ -65,22 +70,16 @@ function LandingScreen({ route ,navigation }) {
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
               >
-                {/*passing props for image and fact*/}
-
-                <FunFact
-                  factImage={require("../assets/factSlides/tiger.jpg")}
-                  funFact="Tigers are the largest cat species in the world reaching up to 3.3 meters in length and weighing up to 670 pounds!"
-                />
-
-                <FunFact
-                  factImage={require("../assets/factSlides/chimpanzee.jpg")}
-                  funFact="Humans and chimpanzees share 95 to 98 percent of the same DNA."
-                />
-
-                <FunFact
-                  factImage={require("../assets/factSlides/elephant.jpg")}
-                  funFact="Elephants use their trunks to suck up water to drink, it can contain up to 8 litres of water. They also use their trunks as a snorkel when swimming."
-                />
+                {/* passing random facts to FunFact Component */}
+                {newImages.map((e) => {
+                  return (
+                    <FunFact
+                      key={e.fact}
+                      factImage={e.referenceimage}
+                      funFact={e.fact}
+                    />
+                  );
+                })}
               </ScrollView>
             </View>
           </View>
@@ -92,7 +91,7 @@ function LandingScreen({ route ,navigation }) {
         <TouchableOpacity
           onPress={() =>
             //setimg() -- function call to set image state
-            navigation.navigate("GameScreen",{ GameLobby: GameLobby })
+            navigation.navigate("GameScreen", { GameLobby: GameLobby })
           }
         >
           <Image source={require("../assets/icons/Play.png")} />
