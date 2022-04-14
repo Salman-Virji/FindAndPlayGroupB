910d0e56a23f9d573b93590acb867fc48c04ab6a
arianneguedes <76008566+arianneguedes@users.noreply.github.com>
2022-04-14 14:51:54 -0600

    Minor changes for the presentation
    Minor changes and some comments

--- app/config/Axios.js
+++ app/config/Axios.js
@@ -8,9 +8,10 @@ import axios from "axios";
 
 // const yourIP_Address = 'http://0811-98-125-242-225.ngrok.io';
 // const yourIP_Address = 'http://192.167.2.154:3000';
-//const yourIP_Address = 'http://10.0.0.179:3000'; //Arianne
+// const yourIP_Address = 'http://10.0.0.179:3000'; // Arianne
+const yourIP_Address = 'http://10.0.0.122:3000'; // ???
 
 const BackendQuery = axios.create({
-  baseURL: "http://10.0.0.122:3000",
+  baseURL: yourIP_Address,
 });
 export default BackendQuery;
